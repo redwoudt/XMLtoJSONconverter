de3b54c4b7dcd1ce34b565f08c1a4046c5831203
Redelinghuys <fre04@ostml0204667.bskyb.com>
2014-11-04 09:18:14 +0000

    Initial Commit

---
+++ BytePlay/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  BytePlay
+//
+//  Created by Redelinghuys, Ferdinand on 04/11/2014.
+//  Copyright (c) 2014 Redelinghuys, Ferdinand. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
